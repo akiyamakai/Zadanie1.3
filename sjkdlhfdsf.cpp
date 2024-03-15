2e1ce0df6c8b4a3500866775ee96596c29a45582
akiyamakai <grusha.cs@gmail.com>
2024-03-15 09:12:49 +0200

    Добавьте файлы проекта.

---
+++ sjkdlhfdsf.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main() {
+    const int SIZE = 5;
+    int arr[SIZE] = { 5, 2, 8, 1, 9 };
+    int target = 8;
+
+    bool found = false;
+
+
+    for (int i = 0; i < SIZE; ++i) {
+        if (arr[i] == target) {
+            found = true;
+            break;
+        }
+    }
+
+
+    if (found) {
+        std::cout << "Element " << target << " found in array." << std::endl;
+    }
+    else {
+        std::cout << "Elemnt " << target << "not found in array." << std::endl;
+    }
+
+    return 0;
+}
